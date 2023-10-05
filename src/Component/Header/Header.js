67f1fa567dffa42db05fc8e15f79e31494c2532b
solbinsol <qkaejwnj@naver.com>
2023-10-05 16:32:33 +0900

    CCG 함수
    클릭시 부산지역 변하게하기
    
    마지막폴리곤인 광주에는 되는데
    영어이름이 부산인 곳에는 적용이 안됨

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,31 +1,137 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState ,useRef } from "react";
 import style from "./Header.module.css";
+import Link from "next/link";
+import { useRouter } from 'next/router';
 
-const Header = ({ imageFiles }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  // "다음" 버튼을 클릭할 때 다음 이미지로 이동하는 함수
-  const handleNextClick = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageFiles.length);
+const Header = ({ imageFiles,onAreaClick  }) => {
+  const router = useRouter();
+  const [seoulClicked, setSeoulClicked] = useState(false); // State to track if "서울" is clicked
+
+  const [features, setFeatures] = useState([]); // 폴리곤 데이터를 상태로 관리
+
+const seoulTextRef = useRef(null);
+
+  
+  // 각 이미지 박스마다 독립적인 상태를 가지도록 배열을 사용
+
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  const [currentImageIndices, setCurrentImageIndices] = useState(
+    imageFiles.length > 0 ? Array.from({ length: imageFiles.length }, () => 0) : []
+  );
+
+
+
+  useEffect(() => {
+
+  
+    // 이미지 파일이 추가되면 초기화
+    if (imageFiles.length > 0) {
+      setCurrentImageIndices(Array.from({ length: imageFiles.length }, () => 0));
+
+      // 이미지 로드가 완료되면 imagesLoaded 상태를 true로 설정
+      const imagePromises = imageFiles.map((item) => {
+        return new Promise((resolve) => {
+          const img = new Image();
+          img.src = item.imagePath;
+          img.onload = resolve;
+        });
+      });
+
+      Promise.all(imagePromises)
+        .then(() => {
+          setImagesLoaded(true);
+        })
+        .catch((error) => {
+          console.error("Error loading images:", error);
+        });
+    }
+  }, [imageFiles]);
+  // 특정 이미지 박스의 "다음" 버튼을 클릭할 때 다음 이미지로 이동하는 함수
+  const handleNextClick = (index) => {
+    setCurrentImageIndices((prevIndices) =>
+      prevIndices.map((prevIndex, i) =>
+        i === index ? (prevIndex + 1) % imageFiles.length : prevIndex
+      )
+    );
+    console.log("Next - prevIndices:", currentImageIndices, "index:", index);
   };
+
+  // 특정 이미지 박스의 "이전" 버튼을 클릭할 때 이전 이미지로 이동하는 함수
+  const handlePrevClick = (index) => {
+    setCurrentImageIndices((prevIndices) =>
+      prevIndices.map((prevIndex, i) =>
+        i === index
+          ? prevIndex === 0
+            ? imageFiles.length - 1
+            : prevIndex - 1
+          : prevIndex
+      )
+    );
+    console.log("Prev - prevIndices:", currentImageIndices, "index:", index);
+  };
+  
+
+  const handleSeoulClick = () => {
+    setSeoulClicked(true);
+    // 이후에 필요한 작업을 수행할 수 있습니다.
+  };
+
+  console.log("currentImageIndices:", currentImageIndices);
+// handleNextClick 함수 내에 추가
+// handlePrevClick 함수 내에 추가
+
+const handleLogoClick = () => {
+  router.reload(); // 현재 페이지를 다시 로드하여 새로고침합니다.
+};
+
+
 
   return (
     <div className={style.Header}>
       <div className={style.LogoBox}>
-        <h1>LOGO</h1>
-      </div>
+      <h1 className={style.Logo} onClick={handleLogoClick}>
+          Map Photo
+        </h1>      </div>
+      {!imagesLoaded && (
+        <div className={style.MainHeader}>
+<h1
+  style={{ color: seoulClicked ? 'red' : 'black' }}
+  onClick={handleSeoulClick}
+>
+            서울
+          </h1>
+          <h1>부산</h1>
+        </div>
+      )}
       {imageFiles.map((item, index) => (
         <div className={style.ImgBox} key={index}>
           {/* 선택한 지역의 이미지를 동적으로 렌더링 */}
+          {item.date && <p className={style.date}> {item.date}</p>}
+
           <div>
-            <button className={style.NextBtn} onClick={handleNextClick}>
+            <button
+              className={style.PrevBtn}
+              onClick={() => handlePrevClick(index)}
+            >
+              이전
+            </button>
+            <button
+              className={style.NextBtn}
+              onClick={() => handleNextClick(index)}
+            >
               다음
             </button>
             <img
-              src={currentImageIndex === index ? item.imagePath2 : item.imagePath}
+              src={
+                currentImageIndices[index] === index
+                  ? item.imagePath2
+                  : item.imagePath
+              }
               alt={`Header Image ${index}`}
             />
-            {item.neighborhood && <p>Neighborhood: {item.neighborhood}</p>}
+            {item.neighborhood && <p className={style.neighborhood}> {item.neighborhood}</p>}
           </div>
         </div>
       ))}
--- src/Component/KakaoMap/KaKaoMap.js
+++ src/Component/KakaoMap/KaKaoMap.js
@@ -1,17 +1,33 @@
 // pages/index.js
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useEffect, useState ,useRef  } from "react";
 import style from "./map.module.css";
 import Header from "../Header/Header";
 
 export default function Map({ latitude, longitude }) {
   const apiKey = "a32953360d2d9f9db8455072180a1a51";
+
+  const [polygon, setPolygon] = useState(); // 폴리곤 객체 상태 추가
+
   const [selectedArea, setSelectedArea] = useState(null);
   const [imageFiles, setImageFiles] = useState([]);
+  const [currentImageIndices, setCurrentImageIndices] = useState(
+    imageFiles.length > 0 ? Array.from({ length: imageFiles.length }, () => 0) : []
+  );
+
+  const [features, setFeatures] = useState([]); // 폴리곤 데이터를 상태로 관리
+
+  const seoulTextRef = useRef(null); // 서울 텍스트 엘리먼트의 ref
 
   useEffect(() => {
+
+
+
+    
     const script = document.createElement("script");
-    script.async = true;
+    // 이곳 script.async = true;
+    script.async = false;
+
     script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&autoload=false`;
     document.head.appendChild(script);
 
@@ -40,9 +56,10 @@ export default function Map({ latitude, longitude }) {
                 strokeWeight: 1,
                 strokeColor: '#39DE2A',
                 strokeOpacity: 0.8,
-                fillColor: '#A2FF99',
+                fillColor: feature.properties.CTP_ENG_NM === "Seoul" ? "#FF0000" : "#A2FF99", // Change fillColor to red for Seoul
                 fillOpacity: 0.5
               });
+              polygon.ctpEngNm = feature.properties.CTP_ENG_NM;
 
               polygon.setMap(map);
 
@@ -63,9 +80,14 @@ export default function Map({ latitude, longitude }) {
                 // 폴리곤 위에서 마우스가 벗어났을 때 처리할 코드
                 // fillColor를 원래대로 돌리는 코드
                 polygon.setOptions({
-                  fillColor: '#A2FF99',
+                  fillColor: feature.properties.CTP_ENG_NM === "Seoul" ? "#FF0000" : "#A2FF99", // Change fillColor to red for Seoul
                 });
               });
+              //console.log(polygon);
+
+
+              
+
 
               // 폴리곤에 클릭 이벤트 핸들러 추가
               window.kakao.maps.event.addListener(polygon, 'click', function() {
@@ -83,7 +105,109 @@ export default function Map({ latitude, longitude }) {
           });
       });
     });
+
+
   }, []);
+  const CCG = () => {
+    console.log("Ss");
+  
+    // 변경할 폴리곤의 ctpEngNm 값을 설정
+    const targetCtpEngNm = "Busan";
+  
+    // 모든 폴리곤을 순회하며 Busan인 경우에만 색상을 변경
+    features.forEach(feature => {
+      const properties = feature.properties;
+      console.log("sssss");
+      if (properties.CTP_ENG_NM === targetCtpEngNm) {
+        console.log("포문" + properties.CTP_ENG_NM);
+  
+        // 폴리곤의 fillColor를 변경하려면 해당 폴리곤 객체에 접근해야 합니다.
+        // 현재 코드에서는 폴리곤 객체가 어떤 변수에 저장되는지 확인이 필요합니다.
+        // 아래 예시에서는 feature.polygon으로 가정하겠습니다.
+  
+        const polygonToChange = feature;
+  
+        // 폴리곤 객체의 fillColor를 변경
+        polygonToChange.setOptions({
+          fillColor: "pink"
+        });
+      }
+    });
+  };
+  
+  
+  
+  
+  
+
+  /*
+  const CCG = () => {
+    console.log("sds");
+    if (polygon) {
+      // 현재 폴리곤의 옵션을 가져옵니다.
+      const currentStrokeWeight = polygon.strokeWeight;
+      const currentStrokeColor = polygon.strokeColor;
+      const currentStrokeOpacity = polygon.strokeOpacity;
+      const currentFillColor = polygon.fillColor;
+      const currentFillOpacity = polygon.fillOpacity;
+  
+      // 변경할 옵션 값을 설정합니다.
+      const newFillColor = "blue"; // 변경하고자 하는 색상
+  
+      // 새로운 옵션을 적용하여 폴리곤을 업데이트합니다.
+      polygon.setOptions({
+        strokeWeight: currentStrokeWeight,
+        strokeColor: currentStrokeColor,
+        strokeOpacity: currentStrokeOpacity,
+        fillColor: newFillColor,
+        fillOpacity: currentFillOpacity,
+      });
+    }
+
+    console.log("Polygon updated:", polygon.fillColor);
+
+  };
+  */
+  
+  
+  // 선택한 지역에 대한 이미지 인덱스 배열을 반환하는 함수
+const getImageIndicesForArea = (areaName) => {
+  // imageFiles에서 해당 지역에 대한 이미지 파일들의 인덱스를 찾아서 반환
+  const indices = imageFiles
+    .map((item, index) => (item.area === areaName ? index : -1))
+    .filter((index) => index !== -1);
+  return indices;
+};
+
+
+// pages/index.js
+
+// ...
+
+const handleAreaClick = (areaName) => {
+  setSelectedArea(areaName);
+
+  // 이미지 인덱스 설정 및 이미지 로드
+  setCurrentImageIndices(getImageIndicesForArea(areaName));
+
+  // 각 폴리곤의 "CTP_ENG_NM"과 비교하여 폴리곤을 식별
+  features.forEach(feature => {
+    const properties = feature.properties;
+    if (properties.CTP_ENG_NM === areaName) {
+      // 폴리곤 색상 변경 등의 처리
+      // ...
+
+      seoulTextRef.current.style.color = "red"; // 빨간색으로 변경
+
+    }
+  });
+
+  console.log(areaName);
+};
+
+// ...
+
+
 
   // 선택한 지역에 따라 해당 지역의 이미지 파일 목록을 가져오는 함수
   const getImagePathsForArea = (areaName) => {
@@ -105,11 +229,14 @@ export default function Map({ latitude, longitude }) {
       <div>
         <div className={style.LeftHeader}>
           {/* 이미지 파일 목록을 헤더 컴포넌트로 전달 */}
-          <Header imageFiles={imageFiles} />
+          <Header imageFiles={imageFiles} selectedArea={selectedArea}  onAreaClick={handleAreaClick} />
         </div>
         <div id="map" className={style.Map}></div>
         <div></div>
+        <button className={style.BBB} onClick={CCG}>sdsds</button>
+
       </div>
     </>
+
   );
 }
